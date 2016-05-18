ce0a66900682c3681e4062a9822ded95e45c6660
Paul Gerlich <paul.gerlich@workiva.com>
2016-05-17 23:48:19 -0500

    Updated analytics.

--- js/schedule.js
+++ js/schedule.js
@@ -9,7 +9,7 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
     const STATUS_COMPLETE = 2;
     const STATUS_PAID = 3;
 
-     //Initialize Parse
+    //Initialize Parse
     Parse.$ = jQuery;
     Parse.initialize("Nw7LzDSBThSKyvym6Q7TwDWcRcz44aOddL75efLL", "ZQPEog0nlJgVwBSbHRfgiGeWNTczY8Lr7PXeUWMU");
 
@@ -27,35 +27,35 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
         getWashesForUser();
     }
 
-     /**
-        Gets vehicles associated with a user
-    */
+    /**
+     Gets vehicles associated with a user
+     */
     function getVehiclesForUser(){
         //Setup query to grab the vehicles associated with this customer
         var query = new Parse.Query("Vehicles");
         query.equalTo("customer", {
-                __type: "Pointer",
-                className: "_User",
-                objectId: $scope.user.id
-            });
+            __type: "Pointer",
+            className: "_User",
+            objectId: $scope.user.id
+        });
 
         vehicles = []; //Vehicles array
 
         //Grab vehicles
         query.find({
-          success: function(results) {
-            for(var i = 0; i < results.length; i++ ) {
-                var vehicle = {make: results[i].get("make"), model: results[i].get("model"), year: results[i].get("year"), color: results[i].get("color"), parseId: results[i].id, pointer: results[i]}
-                $scope.vehicles.push(vehicle)
-            }
+            success: function(results) {
+                for(var i = 0; i < results.length; i++ ) {
+                    var vehicle = {make: results[i].get("make"), model: results[i].get("model"), year: results[i].get("year"), color: results[i].get("color"), parseId: results[i].id, pointer: results[i]}
+                    $scope.vehicles.push(vehicle)
+                }
 
-            $scope.vehiclesLoaded = true;
-            $scope.$apply();
-            
-          },
-          error: function(error) {
-            alert("Error: " + error.code + " " + error.message); //TODO: Error handling
-          }
+                $scope.vehiclesLoaded = true;
+                $scope.$apply();
+
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message); //TODO: Error handling
+            }
         });
     }
 
@@ -64,10 +64,10 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
         //Setup query to grab the washes associated with this user
         var query = new Parse.Query("Jobs");
         query.equalTo("customer", {
-                __type: "Pointer",
-                className: "_User",
-                objectId: $scope.user.id
-            });
+            __type: "Pointer",
+            className: "_User",
+            objectId: $scope.user.id
+        });
         query.notEqualTo("status", 3); //If the wash isn't complete
         query.notEqualTo("status", -1); //If the wash wasn't cancelled
 
@@ -76,67 +76,67 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
 
         //Grab vehicles
         query.find({
-          success: function(results) {
-            $scope.jobs = [];
+            success: function(results) {
+                $scope.jobs = [];
 
-            for(var i = 0; i < results.length; i++ ) {
-                var wash = results[i];
-                var job = {date: wash.get("date"), time: wash.get("time"), status: convertStatusToString(wash.get("status")), washId: wash.get("washId"), employeeNotes: wash.get("employeeNotes"), type: wash.get("vehicle").get("type"), pointer: wash};
-                var beforePicture = wash.get("beforePicture");
-                var afterPicture = wash.get("afterPicture");
-                var employee = wash.get("employee");
-                
-                //Add before/after picture if applicable
-                if ( beforePicture ) {
-                    job.beforePictureURL = beforePicture.url();
+                for(var i = 0; i < results.length; i++ ) {
+                    var wash = results[i];
+                    var job = {date: wash.get("date"), time: wash.get("time"), status: convertStatusToString(wash.get("status")), washId: wash.get("washId"), employeeNotes: wash.get("employeeNotes"), type: wash.get("vehicle").get("type"), pointer: wash};
+                    var beforePicture = wash.get("beforePicture");
+                    var afterPicture = wash.get("afterPicture");
+                    var employee = wash.get("employee");
+
+                    //Add before/after picture if applicable
+                    if ( beforePicture ) {
+                        job.beforePictureURL = beforePicture.url();
+                    }
+
+                    if ( afterPicture ) {
+                        job.afterPictureURL = afterPicture.url();
+                    }
+
+                    if ( employee ) {
+                        job.employee = employee.get("stripeAccount");
+                    }
+
+                    //Whether or not this job is complete (for displaying pay/cancel)
+                    job.complete = wash.get("status") == STATUS_COMPLETE;
+                    job.paid = wash.get("status") == STATUS_PAID;
+
+                    $scope.jobs.push(job)
                 }
 
-                if ( afterPicture ) {
-                    job.afterPictureURL = afterPicture.url();
-                }
+                $scope.jobsLoaded = true;
 
-                if ( employee ) {
-                    job.employee = employee.get("stripeAccount");
-                }
+                $scope.$apply();
 
-                //Whether or not this job is complete (for displaying pay/cancel)
-                job.complete = wash.get("status") == STATUS_COMPLETE;
-                job.paid = wash.get("status") == STATUS_PAID;
-                
-                $scope.jobs.push(job)
+                setTimeout(getWashesForUser, 2000); // Do every two seconds to ensure concurrency
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message); //TODO: Error handling
             }
-
-            $scope.jobsLoaded = true;
-
-            $scope.$apply();
-
-            setTimeout(getWashesForUser, 2000); // Do every two seconds to ensure concurrency
-          },
-          error: function(error) {
-            alert("Error: " + error.code + " " + error.message); //TODO: Error handling
-          }
         });
     }
 
     $scope.payForJob = function(job){
-		//Display Payment Modal
-	    var modalInstance = $uibModal.open({
-	      animation: true,
-	      templateUrl: 'jobCompletionModal.html',
-	      controller: 'jobCompletionCtrl',
-          windowClass: 'adjustModalHeight',
-          resolve: {
-            job: job,
-            cards: null, //TODO: GET USER cards
-          }
-	    });
+        //Display Payment Modal
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'jobCompletionModal.html',
+            controller: 'jobCompletionCtrl',
+            windowClass: 'adjustModalHeight',
+            resolve: {
+                job: job,
+                cards: null, //TODO: GET USER cards
+            }
+        });
 
-	    //Upon pressing save, save the job (after validating input below)
-	    modalInstance.result.then(function (newJob) {
-	    	$scope.createJobTransaction(newJob);
-	    }, function () {
-	    	//Modal was closed instead of saved
-	    });
+        //Upon pressing save, save the job (after validating input below)
+        modalInstance.result.then(function (newJob) {
+            $scope.createJobTransaction(newJob);
+        }, function () {
+            //Modal was closed instead of saved
+        });
 
     }
 
@@ -148,7 +148,7 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
         var ccid = job.card;
         var transactionURL = "php/payForWash.php?TIPAMT="+tip+"&BASEAMT="+baseCost+"&CID="+cid+"&AID="+aid+"&CCID="+ccid;
 
-         $.get( transactionURL, function( data ) {
+        $.get( transactionURL, function( data ) {
 
             var fraudDetected = data.indexOf("fraudulent charge detected.");
 
@@ -160,7 +160,7 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
                     $scope.completeJob(job);
                 }
             }
-         });
+        });
 
     }
 
@@ -168,14 +168,14 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
         job.pointer.set("status", STATUS_PAID);
 
         job.pointer.save(null, {
-        success: function(newJob) {
-            alert("Payment complete. Thank you for your business!");
-            $scope.recordEmployeeAnalytic(newJob);
-            $scope.recordJobAnalytic(job);
-        },
-        error: function(newJob, error) {
-          alert('Failed to pay for job with error code: \"' + error.message + ' \" Please try again later.');
-        }
+            success: function(newJob) {
+                alert("Payment complete. Thank you for your business!");
+                $scope.recordEmployeeAnalytic(newJob);
+                $scope.recordJobAnalytics(newJob);
+            },
+            error: function(newJob, error) {
+                alert('Failed to pay for job with error code: \"' + error.message + ' \" Please try again later.');
+            }
         });
     }
 
@@ -202,18 +202,100 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
         });
     }
 
-    $scope.recordJobAnalytic = function(job) {
+    $scope.recordJobAnalytics = function(job) {
+        $scope.timeAnalytic(job);
+        $scope.dayAnalytic(job);
+        $scope.sedanAnalytic(job);
+    }
+
+    $scope.timeAnalytic = function(job){
+        var timeString = job.get("time").split(' ')[0];
+        timeString = timeString.split(':')[0] + timeString.split('00')[1];
+
         var query = new Parse.Query("WashAnalytics");
-        query.equalTo("stat", job.get("time").split(' ')[0]);
+        query.equalTo("stat", timeString);
 
         query.find({
             success: function (jobAnalytic) {
                 var analytic = jobAnalytic[0];
-                analytic.set('value', Integer.valueOf(analytic.get('value')) + 1);
+
+                analytic.set('value', String(Number(analytic.get('value')) + 1));
 
                 analytic.save(null, {
-                    success: function(newJob) {
+                    success: function(newAnalytic) {
                         //TODO: Saved succesfully
+                    }
+                });
+            }
+        });
+    }
+
+    $scope.dayAnalytic = function(job){
+        var date = new Date(job.get('date'));
+        var day = date.getDay();
+        var analyticString;
+
+        var weekday = new Array(7);
+        weekday[0]=  "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        analyticString = weekday[day] + 'Washes';
+
+        var query = new Parse.Query("WashAnalytics");
+        query.equalTo("stat", analyticString);
+
+        query.find({
+            success: function (jobAnalytic) {
+                var analytic = jobAnalytic[0];
+                analytic.set('value', String(Number(analytic.get('value')) + 1));
+
+                analytic.save(null, {
+                    success: function(newAnalytic) {
+                        //TODO: Saved succesfully
+                    }
+                });
+            }
+        });
+    }
+
+    $scope.sedanAnalytic = function(job){
+        var query = new Parse.Query("Vehicles");
+        query.equalTo("objectId", job.get('vehicle').id);
+
+        query.find({
+            success: function (vehicles) {
+                var vehicle = vehicles[0];
+                var analyticString;
+
+                switch(Number(vehicle.get('type'))) {
+                    case 0:
+                        analyticString = 'Sedan';
+                        break;
+                    case 1:
+                        analyticString = 'NonSedan'
+                        break;
+                }
+
+                analyticString += 'Washes';
+
+                var query = new Parse.Query("WashAnalytics");
+                query.equalTo("stat", analyticString);
+
+                query.find({
+                    success: function (jobAnalytic) {
+                        var analytic = jobAnalytic[0];
+                        analytic.set('value', String(Number(analytic.get('value')) + 1));
+
+                        analytic.save(null, {
+                            success: function(newAnalytic) {
+                                //TODO: Saved succesfully
+                            }
+                        });
                     }
                 });
             }
@@ -222,21 +304,21 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
 
     //Claim a job
     $scope.cancelWash = function(job){
-      job.pointer.set("status", -1);
+        job.pointer.set("status", -1);
 
-      job.pointer.save(null, {
-        success: function(newJob) {
-            //Succeeded
-        },
-        error: function(newJob, error) {
-          alert('Failed to cancel job, with error code: ' + error.message + ' Please try again later.');
-        }
-      });
+        job.pointer.save(null, {
+            success: function(newJob) {
+                //Succeeded
+            },
+            error: function(newJob, error) {
+                alert('Failed to cancel job, with error code: ' + error.message + ' Please try again later.');
+            }
+        });
 
     }
 
     function convertStatusToString(status){
-        if (status == STATUS_UNCLAIMED) { 
+        if (status == STATUS_UNCLAIMED) {
             return "Unclaimed";
         } else if ( status == STATUS_CLAIMED ) {
             return "Claimed";
@@ -254,35 +336,35 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
 
         //Grab vehicles
         query.find({
-          success: function(results) {
-            $scope.washId = 0;
-            if ( results.length == 1 ) {
-                $scope.washId = results[0].get("jobCount");
-                
-                results[0].set("jobCount", $scope.washId + 1);
-                results[0].save(null, {
-                    success: function(newJob) {
-                        //Succeess
-                    },
-                    error: function(newJob, error) {
-                      //Failure
-                    }
-                });
+            success: function(results) {
+                $scope.washId = 0;
+                if ( results.length == 1 ) {
+                    $scope.washId = results[0].get("jobCount");
+
+                    results[0].set("jobCount", $scope.washId + 1);
+                    results[0].save(null, {
+                        success: function(newJob) {
+                            //Succeess
+                        },
+                        error: function(newJob, error) {
+                            //Failure
+                        }
+                    });
 
                     //No Lat/long --> Need to get it
-                 if ( $scope.lat == 0 && $scope.lon == 0 ) {
-                    $scope.convertAddressToLatLng();
+                    if ( $scope.lat == 0 && $scope.lon == 0 ) {
+                        $scope.convertAddressToLatLng();
+                    } else {
+                        $scope.scheduleWash(); //Else --> Go straight to scheduling
+                    }
+
                 } else {
-                    $scope.scheduleWash(); //Else --> Go straight to scheduling
-                }               
-                
-            } else {
-                alert("Error scheduling wash.");
+                    alert("Error scheduling wash.");
+                }
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message); //TODO: Error handling
             }
-          },
-          error: function(error) {
-            alert("Error: " + error.code + " " + error.message); //TODO: Error handling
-          }
         });
     }
 
@@ -309,15 +391,15 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
         var geocodingURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "," + city + "," + state + "&key=AIzaSyCRCnKRU2C6OAahaKiV_r21CzMqolt3iH4"; //TODO: OBFUSCATE
         $.get( geocodingURL, function( data ) {
 
-          //Basic error catching
-          if ( !data['status'] == 'OK' ) {
-            console.log("error occured");
-          } else {
-            $scope.lat = data.results[0].geometry.location.lat;
-            $scope.lon = data.results[0].geometry.location.lng;
+            //Basic error catching
+            if ( !data['status'] == 'OK' ) {
+                console.log("error occured");
+            } else {
+                $scope.lat = data.results[0].geometry.location.lat;
+                $scope.lon = data.results[0].geometry.location.lng;
 
-            $scope.scheduleWash();
-          }
+                $scope.scheduleWash();
+            }
         });
     }
 
@@ -347,7 +429,7 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
             mapTypeControl:false,
             navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
         }
-        
+
         var map = new google.maps.Map(document.getElementById("mapHolder"), myOptions);
 
         //Setup the marker
@@ -362,10 +444,10 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
         });
 
         //On drag, update lat/lon
-         marker.addListener('dragend', function() {
+        marker.addListener('dragend', function() {
             $scope.lat = marker.getPosition().lat();
             $scope.lon = marker.getPosition().lng();
-          });
+        });
 
     }
 
@@ -387,12 +469,12 @@ angular.module("myApp").controller("mainCtrl", function($scope, $uibModal){
 
         //Persist to Parse
         parseJob.save(null, {
-          success: function(newParseJob) {
-            alert('Saved succesfully');
-          },
-          error: function(vehicle, error) {
-            alert('Failed to create new vehicle, with error code: ' + error.message);
-          }
+            success: function(newParseJob) {
+                alert('Saved succesfully');
+            },
+            error: function(vehicle, error) {
+                alert('Failed to create new vehicle, with error code: ' + error.message);
+            }
         });
     }
 
@@ -437,8 +519,8 @@ angular.module('myApp').controller('jobCompletionCtrl', function ($scope, $uibMo
     $scope.displayBaseCost = '$' + ($scope.baseCost/100).toFixed(2);
 
     $scope.ok = function () {
-    	//TODO: Validate inputs
-    	job.tip = $scope.tip * 100;
+        //TODO: Validate inputs
+        job.tip = $scope.tip * 100;
         job.pointer.set("tip", job.tip + "");
         job.pointer.set("rating", $scope.rating);
         job.pointer.set("ratingText", $scope.ratingText);
@@ -459,8 +541,8 @@ angular.module('myApp').controller('jobCompletionCtrl', function ($scope, $uibMo
     };
 
     /**
-    Gets cards associated with a user
-    */
+     Gets cards associated with a user
+     */
     $scope.getCardsForUser = function(){
         $.get( "php/retrieveCustomer.php?CID=" + Parse.User.current().get("stripeAccount"), function( data ) {
             var test = JSON.parse(data.slice(21, data.len));
